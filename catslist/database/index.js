678932a0fff594644b937b383024db8dae59aa35
BrandonALittle <brandon.a.little@gmail.com>
2018-02-24 11:05:24 -0800

    implemented generic database interactions

--- catslist/database/index.js
+++ catslist/database/index.js
@@ -2,18 +2,51 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/catslist');
 
 // create collection schema
+let catSchema = mongoose.Schema({
+  name: String,
+  image: String,
+  owner: String,
+  description: String,
+});
 
 // create model with schema
+let Cat = mongoose.model('Cat', catSchema);
 
 // create methods for db interaction
 
 // save() - insert a new document into the collection
+let save = (cat) => {
+  Cat.create(cat, function(err, newCat) {
+    if (err) return handleError(err);
+    console.log('Cat added to database');
+  });
+}
 
 // remove() - remove a document from the collection
+let remove = (cat) => {
+  Cat.remove(cat, function(err, removedCat) {
+    if (err) return handleError(err);
+    console.log('Cat removed from database');
+  });
+}
 
 // retrieve() - retrieve a document from the collection
+let retrieve = (cat, callback) => {
+  Cat.find(cat, function(err, results) {
+    if (err) return handleError(err);
+    console.log(results);
+    callback(results);
+  })
+}
 
 // retrieveAll() - retrieve all documents from the collection
+let retrieveAll = (callback) => {
+  Cat.find({}, function(err, results) {
+    if (err) return handleError(err);
+    console.log(results);
+    callback(results);
+  })
+}
 
 module.exports.save = save;
 module.exports.remove = remove;
